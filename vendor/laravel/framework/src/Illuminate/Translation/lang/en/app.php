b3ff607933716488139fbe9315a3a81641cba86b
NijiGon <matthewarif50@gmail.com>
2024-01-26 02:58:32 +0700

    add vendor and env

---
+++ vendor/laravel/framework/src/Illuminate/Translation/lang/en/app.php
@@ -0,0 +1,48 @@
+<?php
+return [
+    'layout' => [
+        'home' => 'Home',
+        'cart' => 'Cart',
+        'maintenance' => 'Account Maintenance',
+        'profile' => 'Profile',
+        'lang' => 'Set Language',
+        'login' => 'Login',
+        'register' => 'Register',
+        'logout' => 'Logout'
+    ],
+    'form' => [
+        'first' => 'First Name',
+        'last' => 'Last Name',
+        'email' => 'Email Address',
+        'role' => 'Role',
+        'picture' => 'Display Picture',
+        'gender' => 'Gender',
+        'password' => 'Password',
+        'confirmation' => 'Password Confirmation',
+        'submit' => 'Submit',
+        'select' => 'Select Role',
+        'save' => 'Save Changes',
+    ],
+    'table' => [
+        'name' => 'Name',
+        'description' => 'Description',
+        'price' => 'Price',
+        'action' => 'Action',
+        'detail' => 'Details',
+        'delete' => 'Delete',
+        'total' => 'Total',
+        'checkout' => 'Checkout',
+        'empty' => 'Cart is empty',
+        'account' => 'Account',
+        'update' => 'Update Role',
+    ],
+    'catalog' => [
+        'add' => 'Add to Cart',
+    ],
+    'message' => [
+        'success' => 'Success',
+        'home' => 'Home'
+    ],
+    'lang' => 'EN'
+
+];
---
+++ vendor/laravel/framework/src/Illuminate/Translation/lang/id/app.php
@@ -0,0 +1,48 @@
+<?php
+return [
+    'layout' => [
+        'home' => 'Menu Utama',
+        'cart' => 'Keranjang',
+        'maintenance' => 'Pemeliharaan Akun',
+        'profile' => 'Profil',
+        'lang' => 'Ubah Bahasa',
+        'login' => 'Masuk',
+        'register' => 'Daftar',
+        'logout' => 'Keluar'
+    ],
+    'form' => [
+        'first' => 'Nama Depan',
+        'last' => 'Nama Belakang',
+        'email' => 'Alamat Email',
+        'role' => 'Peran',
+        'picture' => 'Gambar Tampilan',
+        'gender' => 'Jenis Kelamin',
+        'password' => 'Sandi',
+        'confirmation' => 'Konfirmasi Sandi',
+        'submit' => 'Daftar',
+        'select' => 'Pilih Peran',
+        'save' => 'Simpan',
+    ],
+    'table' => [
+        'name' => 'Nama',
+        'description' => 'Deskripsi',
+        'price' => 'Harga',
+        'action' => 'Aksi',
+        'detail' => 'Detail',
+        'delete' => 'Hapus',
+        'total' => 'Total',
+        'checkout' => 'Pembayaran',
+        'empty' => 'Keranjang Kosong',
+        'account' => 'Akun',
+        'update' => 'Ubah Peran',
+    ],
+    'catalog' => [
+        'add' => 'Tambahkan ke Keranjang',
+    ],
+    'message' => [
+        'success' => 'Berhasil',
+        'home' => 'Halaman Utama'
+    ],
+    'lang' => 'ID'
+
+];
